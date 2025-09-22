d05d0094a3f6890b1d89bec61be82aeeb1dce015
Lynstery <hanzheng@smail.nju.edu.cn>
2025-09-23 04:10:56 +0800

    updates

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,50 +1,55 @@
 echo ''
 info 'install zshrc'
 
-SRC="$DOTFILES_ROOT/zsh/zshrc"
-DST="$HOME/.zshrc"
-START="# >>> added by dotfiles >>>"
-END="# <<< added by dotfiles <<<"
+setup_zshrc(){
+  local overwrite=false backup=false skip=false action=
 
-touch "$DST"
+  SRC="$DOTFILES_ROOT/zsh/zshrc"
+  DST="$HOME/.zshrc"
+  START="# >>> added by dotfiles >>>"
+  END="# <<< added by dotfiles <<<"
 
-TMP=$(mktemp)
-{
-  echo "$START"
-  cat "$SRC"
-  echo "$END"
-} > "$TMP"
+  TMP=$(mktemp)
+  {
+    echo "$START"
+    cat "$SRC"
+    echo "$END"
+  } > "$TMP"
 
-cp -f "$DST" "$DST.bak"
+  if [[ -s "$DST" ]]; then
+    user "File already exists: $DST ($(basename "$SRC")). What do you want to do?"
+    echo "[s]kip, [b]ack up & overwrite"
+    read -rn1 action </dev/tty
+    echo ""
 
-awk -v start="$START" -v end="$END" -v replfile="$TMP" '
-  BEGIN {
-    while ((getline l < replfile) > 0) buf = buf l ORS
-    close(replfile)
-    inblk = 0
-    seen = 0
-  }
-  index($0, start) {
-    print buf
-    inblk = 1
-    seen = 1
-    next
-  }
-  index($0, end) {
-    inblk = 0
-    next
-  }
-  inblk == 0 {
-    print
-  }
-  END {
-    if (seen == 0) {
-      print buf
-    }
-  }
-' "$DST" > "$DST.new" && mv "$DST.new" "$DST"
+    case "$action" in
+      b) backup=true ;;
+      s) skip=true ;;
+      *) ;;
+    esac
 
-rm -f "$TMP"
-echo "Updated $DST (backup in $DST.bak)"
+    if [[ $backup == true ]]; then
+      cp -f "$DST" "$DST.bak"
+      awk -v start="$START" -v end="$END" -v replfile="$TMP" '
+        BEGIN{while((getline l<replfile)>0)buf=buf l ORS;close(replfile);inblk=0;seen=0}
+        index($0,start){print buf;inblk=1;seen=1;next}
+        index($0,end){inblk=0;next}
+        inblk==0{print}
+        END{if(seen==0)print buf}
+      ' "$DST.bak" > "$DST"
+      success "updated $DST with backup at $DST.bak"
+    elif [[ $skip == true ]]; then
+      success "skipped $DST"
+    fi
+  else
+    cat "$TMP" > "$DST"
+    success "created $DST"
+  fi
+
+  rm -f "$TMP"
+}
+# 其他额外操作
+rm -f "$HOME/.config/spaceship.zsh.zwc" 2>/dev/null || true
+linkfile "$DOTFILES_ROOT/zsh/spaceship.zsh.symlink" "$HOME/.config/spaceship.zsh"
 
 success 'install zshrc'
--- zsh/zinit_plugins.zsh
+++ zsh/zinit_plugins.zsh
@@ -2,6 +2,15 @@
 
 zinit light zsh-users/zsh-completions
 
+zinit light jeffreytse/zsh-vi-mode
+
+zinit light zdharma-continuum/history-search-multi-word
+zstyle ":history-search-multi-word" highlight-color "fg=yellow,bold"
+zstyle ":history-search-multi-word" page-size "30"
+bindkey -M emacs '\er' history-search-multi-word
+bindkey -M viins '\er' history-search-multi-word
+bindkey -M vicmd '\er' history-search-multi-word
+
 zinit snippet OMZL::functions.zsh
 zinit snippet OMZL::directories.zsh
 zinit snippet OMZL::clipboard.zsh
@@ -22,19 +31,13 @@ autoload -Uz compinit
 compinit -C
 zinit cdreplay -q
 
+zinit light zsh-users/zsh-autosuggestions
+
 zinit light Aloxaf/fzf-tab
 zstyle ':fzf-tab:complete:cd:*'          fzf-preview 'eza --icons -1 --color=always $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*'  fzf-preview 'eza --icons -1 --color=always $realpath'
 zstyle ':fzf-tab:complete:z:*'           fzf-preview 'eza --icons -1 --color=always $realpath'
 
-zinit light zdharma-continuum/history-search-multi-word
-zstyle ":history-search-multi-word" highlight-color "fg=yellow,bold"
-zstyle ":history-search-multi-word" page-size "30"
-bindkey -M emacs '\er' history-search-multi-word
-bindkey -M viins '\er' history-search-multi-word
-bindkey -M vicmd '\er' history-search-multi-word
 
-zinit light zsh-users/zsh-autosuggestions
 zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light spaceship-prompt/spaceship-prompt
-zinit light jeffreytse/zsh-vi-mode
