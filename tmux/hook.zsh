ce6a7310610e13eed7410a71d7d53dfb890ce45c
Lynstery <hanzheng@smail.nju.edu.cn>
2025-09-23 23:47:45 +0800

    updates

--- tmux/hook.zsh
+++ tmux/hook.zsh
@@ -4,9 +4,5 @@ tmux-window-name() {
     (uv run --quiet $TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
   fi
 }
-if ! typeset -f add-zsh-hook >/dev/null; then
-  echo "add-zsh-hook not load"
-else
-  echo "add-zsh-hook loaded"
-  add-zsh-hook chpwd tmux-window-name
-fi
+
+add-zsh-hook chpwd tmux-window-name
