75963ef4aeca6b93c27c388f9fb3235a6606c906
Lynstery <hanzheng@smail.nju.edu.cn>
2025-09-24 10:25:29 +0800

    updates binaries and functions

---
+++ bin/plugin.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+fpath+=( "${0:A:h}" )
+
+#export PATH="${0:A:h}":$PATH
+
+for cmd in ${0:A:h}/*; do
+  [[ -f $cmd ]] || continue
+  [[ $cmd == ${0:A:h}/plugin.zsh ]] && continue
+  [[ ${cmd:t} == _* ]] && continue
+
+  cmd_name=${cmd:t}
+
+  if [[ -f ${0:A:h}/_$cmd_name ]]; then
+    autoload -Uz "_$cmd_name" 2>/dev/null || true
+    compdef "_$cmd_name" "$cmd_name"
+  fi
+done
---
+++ script/install_dependencies.sh
@@ -0,0 +1,15 @@
+# fzf
+sudo apt install fzf
+# nvim
+sudo snap install nvim --classic
+# eza
+sudo apt update
+sudo apt install -y gpg
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+# zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
