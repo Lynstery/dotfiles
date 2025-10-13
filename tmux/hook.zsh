dd288aeeb52883c4be2d4c233bfce8ae9c8fc73d
Lynstery <hanzheng@smail.nju.edu.cn>
2025-10-13 21:15:21 +0800

    tmux

--- tmux/hook.zsh
+++ tmux/hook.zsh
@@ -1,7 +1,7 @@
 # hook for tmux-window-name
 tmux-window-name() {
-  if [[ -n $TMUX_PLUGIN_MANAGER_PATH ]] then
-    (uv run --quiet $TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
+  if [[ -n $TMUX && -x ~/.config/tmux/plugins/tmux-window-name/scripts/rename_session_windows.py ]]; then
+    (uv run --quiet ~/.config/tmux/plugins/tmux-window-name/scripts/rename_session_windows.py &)
   fi
 }
 
--- tmux/install.sh
+++ tmux/install.sh
@@ -3,4 +3,33 @@ info 'install tmux config'
 
 linkfile "$DOTFILES_ROOT/tmux/tmux.symlink" "$HOME/.config/tmux"
 
+TMUX_PLUGINS_PATH=$DOTFILES_ROOT/tmux/tmux.symlink/plugins
+mkdir -p $TMUX_PLUGINS_PATH/catppuccin2
+for repo in \
+  "https://github.com/catppuccin/tmux.git@v2.1.3 $TMUX_PLUGINS_PATH/catppuccin/tmux" \
+  "https://github.com/tmux-plugins/tmux-yank $TMUX_PLUGINS_PATH/tmux-yank" \
+  "https://github.com/tmux-plugins/tmux-logging $TMUX_PLUGINS_PATH/tmux-logging" \
+  "https://github.com/Lynstery/tmux-window-name $TMUX_PLUGINS_PATH/tmux-window-name" \
+  "https://github.com/tmux-plugins/tmux-resurrect $TMUX_PLUGINS_PATH/tmux-resurrect" \
+  "https://github.com/tmux-plugins/tmux-continuum $TMUX_PLUGINS_PATH/tmux-continuum"
+do
+  url="${repo%% *}"
+  path="${repo#* }"
+  branch=""
+  [[ "$url" == *"@"* ]] && branch="-b ${url##*@}" && url="${url%@*}"
+
+  if [ -d "$path/.git" ]; then
+    echo "$path is already cloned"
+    echo "Updating $(basename "$path")..."
+    if git -C "$path" symbolic-ref -q HEAD >/dev/null; then
+      git -C "$path" pull --ff-only
+    else
+      echo "$(basename "$path") is detached (skipping pull)"
+    fi
+  else
+    echo "Cloning $(basename "$path")..."
+    git clone $branch "$url" "$path"
+  fi
+done
+
 success 'install tmux config'
