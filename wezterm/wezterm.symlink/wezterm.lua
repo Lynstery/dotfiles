e52216bd8d1d4e4eac6880968c254a1b8888a0bd
Lynstery <hanzheng@smail.nju.edu.cn>
2025-11-25 16:01:45 +0800

    commits

--- wezterm/wezterm.symlink/wezterm.lua
+++ wezterm/wezterm.symlink/wezterm.lua
@@ -1,124 +1,130 @@
 local wezterm = require("wezterm")
 local config = {
-	font_size = 15,
-	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" }),
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	use_fancy_tab_bar = false,
-	window_decorations = "RESIZE",
-	initial_rows = 40,
-	initial_cols = 130,
-	show_new_tab_button_in_tab_bar = true,
-	window_background_opacity = 1,
-	macos_window_background_blur = 50,
-	text_background_opacity = 0.9,
-	adjust_window_size_when_changing_font_size = false,
-	window_padding = {
-		left = 5,
-		right = 5,
-		top = 5,
-		bottom = 5,
-	},
-	freetype_load_flags = "NO_HINTING",
-	front_end = "WebGpu",
-	--freetype_render_target = "Normal",
-	--freetype_load_target = "Light",
+  font_size = 15,
+  font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" }),
+  color_scheme = "Catppuccin Mocha",
+  enable_tab_bar = false,
+  use_fancy_tab_bar = false,
+  window_decorations = "RESIZE",
+  initial_rows = 40,
+  initial_cols = 130,
+  show_new_tab_button_in_tab_bar = true,
+  window_background_opacity = 1,
+  macos_window_background_blur = 50,
+  text_background_opacity = 0.9,
+  adjust_window_size_when_changing_font_size = false,
+  window_padding = {
+    left = 5,
+    right = 5,
+    top = 5,
+    bottom = 5,
+  },
+  freetype_load_flags = "NO_HINTING",
+  front_end = "WebGpu",
+  --freetype_render_target = "Normal",
+  --freetype_load_target = "Light",
 }
 
+
+if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
+  config.enable_tab_bar = true
+  config.use_fancy_tab_bar = true
+end
+
 config.window_frame = {
-	-- The font used in the tab bar.
-	-- Roboto Bold is the default; this font is bundled
-	-- with wezterm.
-	-- Whatever font is selected here, it will have the
-	-- main font setting appended to it to pick up any
-	-- fallback fonts you may have used there.
-	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font({ family = "Roboto", weight = "Bold" }),
 
-	-- The size of the font in the tab bar.
-	-- Default to 10.0 on Windows but 12.0 on other systems
-	font_size = 12.0,
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 12.0,
 
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = "#333333",
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = "#333333",
 
-	-- The overall background color of the tab bar when
-	-- the window is not focused
-	inactive_titlebar_bg = "#333333",
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = "#333333",
 }
 
 config.colors = {
-	tab_bar = {
-		-- The color of the strip that goes along the top of the window
-		-- (does not apply when fancy tab bar is in use)
-		background = "#0b0022",
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = "#0b0022",
 
-		-- The active tab is the one that has focus in the window
-		active_tab = {
-			-- The color of the background area for the tab
-			bg_color = "#3b2042",
-			-- The color of the text for the tab
-			fg_color = "#c0c0c0",
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      -- The color of the background area for the tab
+      bg_color = "#3b2042",
+      -- The color of the text for the tab
+      fg_color = "#c0c0c0",
 
-			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-			-- label shown for this tab.
-			-- The default is "Normal"
-			intensity = "Bold",
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = "Bold",
 
-			-- Specify whether you want "None", "Single" or "Double" underline for
-			-- label shown for this tab.
-			-- The default is "None"
-			underline = "Double",
+      -- Specify whether you want "None", "Single" or "Double" underline for
+      -- label shown for this tab.
+      -- The default is "None"
+      underline = "Double",
 
-			-- Specify whether you want the text to be italic (true) or not (false)
-			-- for this tab.  The default is false.
-			italic = false,
+      -- Specify whether you want the text to be italic (true) or not (false)
+      -- for this tab.  The default is false.
+      italic = false,
 
-			-- Specify whether you want the text to be rendered with strikethrough (true)
-			-- or not for this tab.  The default is false.
-			strikethrough = true,
-		},
+      -- Specify whether you want the text to be rendered with strikethrough (true)
+      -- or not for this tab.  The default is false.
+      strikethrough = true,
+    },
 
-		-- Inactive tabs are the tabs that do not have focus
-		inactive_tab = {
-			bg_color = "#1b1032",
-			fg_color = "#808080",
+    -- Inactive tabs are the tabs that do not have focus
+    inactive_tab = {
+      bg_color = "#1b1032",
+      fg_color = "#808080",
 
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab`.
-		},
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab`.
+    },
 
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over inactive tabs
-		inactive_tab_hover = {
-			bg_color = "#3b3052",
-			fg_color = "#909090",
-			italic = true,
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over inactive tabs
+    inactive_tab_hover = {
+      bg_color = "#3b3052",
+      fg_color = "#909090",
+      italic = true,
 
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab_hover`.
-		},
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab_hover`.
+    },
 
-		-- The new tab button that let you create new tabs
-		new_tab = {
-			bg_color = "#1b1032",
-			fg_color = "#808080",
+    -- The new tab button that let you create new tabs
+    new_tab = {
+      bg_color = "#1b1032",
+      fg_color = "#808080",
 
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab`.
-		},
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab`.
+    },
 
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over the new tab button
-		new_tab_hover = {
-			bg_color = "#3b3052",
-			fg_color = "#909090",
-			italic = true,
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over the new tab button
+    new_tab_hover = {
+      bg_color = "#3b3052",
+      fg_color = "#909090",
+      italic = true,
 
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab_hover`.
-		},
-	},
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab_hover`.
+    },
+  },
 }
 
 return config
